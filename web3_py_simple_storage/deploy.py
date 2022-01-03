51cc025c58442ebd6f58c8ec09fc45ee703e0a5e
charudatta999 <charudatta.jadhav2299@gmail.com>
2022-01-03 16:39:53 +0530

    stored compiled code

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -1,5 +1,5 @@
 from solcx import compile_standard, install_solc
-
+import json
 
 with open("SmipleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -22,3 +22,5 @@ compiled_sol = compile_standard(
 )
 print(compiled_sol)
 
+with open("compiled_code.json","w") as file:
+    json.dump(compiled_sol,file)
