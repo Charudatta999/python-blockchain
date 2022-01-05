8e5f14c98c05a1d9f41be01175d64706b01b675a
charudatta999 <charudatta.jadhav2299@gmail.com>
2022-01-05 15:30:01 +0530

    getting nonce

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -1,17 +1,17 @@
 from solcx import compile_standard, install_solc
 import json
-from web3 import web3 
+from web3 import Web3 
 
 with open("SmipleStorage.sol", "r") as file:
     simple_storage_file = file.read()
-    print(simple_storage_file)
+    
 # compiled solidity
 
 install_solc("0.6.0")
 compiled_sol = compile_standard(
     {
         "language": "Solidity",
-        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "sources": {"SmipleStorage.sol": {"content": simple_storage_file}},
         "settings": {
             "outputSelection": {
                 "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
@@ -21,14 +21,28 @@ compiled_sol = compile_standard(
     },
     solc_version="0.6.0"
 )
-print(compiled_sol)
 
 with open("compiled_code.json","w") as file:
     json.dump(compiled_sol,file)
 #getting bytecode
-bytecode = compiled_sol["contracts"]["SmipleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
+bytecode = compiled_sol["contracts"]["SmipleStorage.sol"]["SmipleStorage"]["evm"]["bytecode"]["object"]
 
 #getting ABI
 
-abi=compiled_sol["contracts"]["SmipleStorage.sol"]["SimpleStorage"]["abi"]
+abi=compiled_sol["contracts"]["SmipleStorage.sol"]["SmipleStorage"]["abi"]
+
+# requiremnts for conecting to chain
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+chain_id =1337
+my_address ="0xEB20Bbd4F066404Fb68b732D88269B43d5F2f94B"
+private_key ="0x310dfd88cd38333e926b8e04b7b490c858476e3749a9a38ceb497c8fbe655342"
+
+#create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi,bytecode=bytecode)
+print(SimpleStorage)
+
+#get the latest transaction
+
+nonce = w3.eth.getTransactionCount(my_address)
+print(nonce)
 
