ed7315f72fa9d940630dedec2ae0b6b52dc792c1
charudatta999 <charudatta.jadhav2299@gmail.com>
2022-01-08 00:17:44 +0530

    using config file

---
+++ brownie_simple_storage/contracts/SImpleStorage.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
+
+contract SmipleStorage{
+
+    uint256  favouriteNumber;
+
+    function store(uint256 _favNumber) public{
+        favouriteNumber = _favNumber;
+    }
+
+    function retrieve () public view returns(uint256){
+        return favouriteNumber;
+    }
+    struct People{
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    People public person = People({favouriteNumber:1,name:"Charudatta"});
+    //array
+
+    People[] public people;
+
+    //create map
+    mapping(string=> uint256) public nameTOFavNum;
+    //name will be stored in memory
+    
+    function addPerson(string memory _name,uint256 _favNumber) public {
+        people.push(People({favouriteNumber: _favNumber,name:_name}));
+        //creating a map
+        nameTOFavNum[_name] = _favNumber;
+
+    }
+
+}
---
+++ brownie_simple_storage/scripts/deploy.py
@@ -0,0 +1,10 @@
+from brownie import accounts, config
+
+def deploy_simple():
+    #adding account from .env -> config -> config.accounts
+    #account=accounts.add(config["wallets"]["from_key"])
+    print(account)
+    pass
+
+def main():
+    deploy_simple()
