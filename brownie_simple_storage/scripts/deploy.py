c200d56477dc4b984db6ba2a91afdccecba1e6c6
charudatta999 <cjawsome999@gmail.com>
2022-01-08 00:46:55 +0530

    deploying a contract using brownie

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,10 +1,24 @@
-from brownie import accounts, config
+from brownie import accounts, config,SimpleStorage 
 
 def deploy_simple():
     #adding account from .env -> config -> config.accounts
     #account=accounts.add(config["wallets"]["from_key"])
-    print(account)
-    pass
+    account=accounts[0]
+    simple_storage=SimpleStorage.deploy(  {'from': account} )   #deploying contract
+    
+    stored_value=simple_storage.retrieve() #transaction
+    
+    print(stored_value)
+
+    #store transaction
+    transaction=simple_storage.store(15, {'from': account})
+    
+    #how many blocks to wait for the transaction to be mined or get nonce +1
+    transaction.wait(1)
+    
+    stored_value=simple_storage.retrieve() #transaction
+    
+    print(stored_value)
 
 def main():
     deploy_simple()
